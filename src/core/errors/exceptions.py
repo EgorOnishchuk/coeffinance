5ad092a73924942211ffcdd8e18efa9fb9f2784e
Егор <egor_onishchuk@mail.ru>
2025-08-11 16:23:24 +0300

    fix: retrieve the container from a request to avoid cyclic import

--- src/core/errors/exceptions.py
+++
@@ -1,8 +0,0 @@
-class DBConnError(ConnectionError):
-    def __init__(self, msg: str = "Internal error.") -> None:
-        super().__init__(msg)
-
-
-class ExternalAPIError(Exception):
-    def __init__(self, msg: str = "External service error.") -> None:
-        super().__init__(msg)
