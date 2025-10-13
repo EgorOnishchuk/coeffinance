13a12743347f6de1cc6a31c7f84833cbf1ee06ff
Егор <egor_onishchuk@mail.ru>
2025-10-13 18:47:01 +0300

    refactor: improve generic-based service

--- src/users/service.py
+++ src/users/service.py
@@ -1,21 +1,26 @@
-from typing import TYPE_CHECKING
+from typing import cast, override
 
-if TYPE_CHECKING:
-    from dishka import AsyncContainer
-from fastapi import Request
+from fastapi import HTTPException, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_users import BaseUserManager, IntegerIDMixin
 from fastapi_users.db import BaseUserDatabase
-from fastapi_users.exceptions import InvalidResetPasswordToken as LibInvalidResetPasswordToken
+from fastapi_users.exceptions import (
+    InvalidResetPasswordToken as LibInvalidResetPasswordToken,
+)
 from fastapi_users.exceptions import InvalidVerifyToken as LibInvalidVerifyToken
 from fastapi_users.exceptions import UserAlreadyExists as LibUserAlreadyExists
-from fastapi_users.exceptions import UserAlreadyVerified as LibUserAlreadyVerified
+from fastapi_users.exceptions import (
+    UserAlreadyVerified as LibUserAlreadyVerified,
+)
 from fastapi_users.exceptions import UserInactive as LibUserInactive
 from fastapi_users.exceptions import UserNotExists as LibUserNotExists
 from fastapi_users.password import PasswordHelperProtocol
+from fastapi_users.schemas import BaseUserCreate, BaseUserUpdate
 
+from src.core.asgi import ExtendedRequest
 from src.core.settings import AuthSettings
-from src.core.utils.email_clients import EmailClient, Session
+from src.core.utils.mail import MailSession
+from src.users.db.models import DBUserProtocol
 from src.users.errors import (
     AlreadyExistsError,
     AlreadyVerifiedError,
@@ -25,94 +30,128 @@ from src.users.errors import (
     VerificationError,
     WeakPasswordError,
 )
-from src.users.models import SQLAlchemyUser
-from src.users.schemas import UserCreate, UserUpdate
-from src.users.utils.utils import PasswordValidator
+from src.users.schemas import UserCreate
+from src.users.utils.password_validators import PasswordValidator
 
 
-class SQLAlchemyUserManager(IntegerIDMixin, BaseUserManager[SQLAlchemyUser, int]):  # type: ignore[type-var]
+class UserManager(IntegerIDMixin, BaseUserManager[DBUserProtocol, int]):
     """
     Also overrides exception classes via their interception.
     """
 
     def __init__(
         self,
-        user_db: BaseUserDatabase[SQLAlchemyUser, int],  # type: ignore[type-var]
+        user_db: BaseUserDatabase[DBUserProtocol, int],
         settings: AuthSettings,
-        email_client: EmailClient,
         password_validator: PasswordValidator,
         password_helper: PasswordHelperProtocol | None = None,
     ) -> None:
         super().__init__(user_db, password_helper)
 
-        self.settings = settings
+        self._settings = settings
         self.verification_token_secret, self.reset_password_token_secret = (
-            self.settings.email_verification_secret,
-            self.settings.password_reset_secret,
+            self._settings.email_verification_secret,
+            self._settings.password_reset_secret,
         )
-        self.email_client = email_client
-        self.password_validator = password_validator
+        self._password_validator = password_validator
 
+    @override
     async def validate_password(
         self,
         password: str,
-        user: UserCreate | SQLAlchemyUser,  # type: ignore[override]
+        user: BaseUserCreate | DBUserProtocol,
     ) -> None:
-        if not self.password_validator.is_strong(password, related=(user.email, user.nickname)):
-            raise WeakPasswordError(ways_to_solve=self.password_validator.get_improvements(password))
+        # A generic for the user creation schema is not provided by the lib,
+        # but BaseUserCreate is UserCreate here, since it is assigned at the
+        # moment of obtaining APIRouter.
+        # (see «fastapi_users.get_register_router(UserRead, UserCreate)» in
+        # routes.py)
+        user = cast(UserCreate | DBUserProtocol, user)
+        report = self._password_validator.validate(
+            password,
+            related=(user.nickname, user.email),
+        )
 
-    async def on_after_request_verify(  # type: ignore[override]
-        self, user: SQLAlchemyUser, token: str, request: Request
-    ) -> None:
-        container: AsyncContainer = request.app.state.dishka_container
-
-        async with container() as sub_container:
-            await self.email_client.send(
-                session=await sub_container.get(Session),
-                sender=self.settings.sys_email,
-                recipient=user.email,
-                title="Email confirmation",
-                text=f"Please confirm your email address with this code: {token}",
+        if not report.is_strong:
+            raise WeakPasswordError(
+                f"Password is not enough strong: {report.strength}.",
+                ways_to_solve=tuple(report.improvements),
             )
 
-    async def on_after_forgot_password(  # type: ignore[override]
-        self, user: SQLAlchemyUser, token: str, request: Request
+    @override
+    async def on_after_request_verify(
+        self,
+        user: DBUserProtocol,
+        token: str,
+        request: Request | None = None,
     ) -> None:
-        container: AsyncContainer = request.app.state.dishka_container
+        if request is None:
+            raise HTTPException(status_code=500)
+        request = cast(ExtendedRequest, request)
 
-        async with container() as sub_container:
-            await self.email_client.send(
-                session=await sub_container.get(Session),
-                sender=self.settings.sys_email,
+        async with request.app.state.dishka_container() as sub_container:
+            await (await sub_container.get(MailSession)).send(
+                sender=str(self._settings.sys_email),
+                recipient=user.email,
+                title="Email confirmation",
+                text=f"Please confirm your email address with this code: "
+                f"{token}",
+            )
+
+    @override
+    async def on_after_forgot_password(
+        self,
+        user: DBUserProtocol,
+        token: str,
+        request: Request | None = None,
+    ) -> None:
+        if request is None:
+            raise HTTPException(status_code=500)
+        request = cast(ExtendedRequest, request)
+
+        async with request.app.state.dishka_container() as sub_container:
+            await (await sub_container.get(MailSession)).send(
+                sender=str(self._settings.sys_email),
                 recipient=user.email,
                 title="Password reset",
                 text=f"Please reset your password with this code: {token}",
             )
 
+    @override
     async def create(
         self,
-        user_create: UserCreate,  # type: ignore[override]
-        safe: bool = False,  # noqa: FBT001, FBT002
+        user_create: BaseUserCreate,
+        safe: bool = False,
         request: Request | None = None,
-    ) -> SQLAlchemyUser:
+    ) -> DBUserProtocol:
+        if request is None:
+            raise HTTPException(status_code=500)
+
         try:
             return await super().create(user_create, safe, request)
         except LibUserAlreadyExists as exc:
             raise AlreadyExistsError from exc
 
-    async def update(  # type: ignore[override]
+    @override
+    async def update(
         self,
-        user_update: UserUpdate,
-        user: SQLAlchemyUser,
-        request: Request,
-        safe: bool = False,  # noqa: FBT001, FBT002
-    ) -> SQLAlchemyUser:
+        user_update: BaseUserUpdate,
+        user: DBUserProtocol,
+        safe: bool = False,
+        request: Request | None = None,
+    ) -> DBUserProtocol:
+        if request is None:
+            raise HTTPException(status_code=500)
+
         try:
             return await super().update(user_update, user, safe, request)
         except LibUserAlreadyExists as exc:
             raise AlreadyExistsError from exc
 
-    async def authenticate(self, credentials: OAuth2PasswordRequestForm) -> SQLAlchemyUser | None:
+    @override
+    async def authenticate(
+        self, credentials: OAuth2PasswordRequestForm
+    ) -> DBUserProtocol | None:
         user = await super().authenticate(credentials)
 
         if user is None or not user.is_active:
@@ -123,7 +162,10 @@ class SQLAlchemyUserManager(IntegerIDMixin, BaseUserManager[SQLAlchemyUser, int]
 
         return user
 
-    async def verify(self, token: str, request: Request) -> SQLAlchemyUser:  # type: ignore[override]
+    @override
+    async def verify(
+        self, token: str, request: Request | None = None
+    ) -> DBUserProtocol:
         try:
             return await super().verify(token, request)
         except (LibInvalidVerifyToken, LibUserNotExists) as exc:
@@ -131,9 +173,13 @@ class SQLAlchemyUserManager(IntegerIDMixin, BaseUserManager[SQLAlchemyUser, int]
         except LibUserAlreadyVerified as exc:
             raise AlreadyVerifiedError from exc
 
-    async def reset_password(  # type: ignore[override]
-        self, token: str, password: str, request: Request
-    ) -> SQLAlchemyUser:
+    @override
+    async def reset_password(
+        self,
+        token: str,
+        password: str,
+        request: Request | None = None,
+    ) -> DBUserProtocol:
         try:
             return await super().reset_password(token, password, request)
         except (
