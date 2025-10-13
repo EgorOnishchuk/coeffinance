5b4a87067306db7fc1d8082804f987b48c914678
Егор <egor_onishchuk@mail.ru>
2025-10-13 18:43:43 +0300

    feat: add advanced versioning support

--- src/core/middlewares.py
+++ src/core/middlewares.py
@@ -1,19 +1,36 @@
-from fastapi import Request, Response
-from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
-from starlette.types import ASGIApp
+from typing import Any
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from starlette.middleware import (
+    _MiddlewareFactory,  # pyright: ignore[reportPrivateUsage]
+)
+
+from src.core.settings import (
+    CompressionSettings,
+    CORSSettings,
+    TrustedHostsSettings,
+)
+
+#  A factory params and a settings dict must match. This cannot be
+#  expressed through annotation, as the dict cannot be annotated via
+#  P.kwargs.
+type Middleware = tuple[_MiddlewareFactory[Any], dict[str, Any]]
 
 
-class VersionMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app: ASGIApp, version: str) -> None:
-        super().__init__(app)
-        self.version = version
-
-    async def dispatch(
-        self,
-        request: Request,
-        call_next: RequestResponseEndpoint,
-    ) -> Response:
-        response = await call_next(request)
-        response.headers["X-Version"] = self.version
-
-        return response
+def get_middleware_map() -> tuple[Middleware, ...]:
+    return tuple(
+        (  # type: ignore[misc] # A MyPy limitation when dealing with MiddlewareFactory, not confirmed by Pyright.
+            middleware,
+            settings.load().model_dump(
+                by_alias=True,
+                exclude_none=True,
+            ),
+        )
+        for middleware, settings in (
+            (TrustedHostMiddleware, TrustedHostsSettings),
+            (CORSMiddleware, CORSSettings),
+            (GZipMiddleware, CompressionSettings),
+        )
+    )
